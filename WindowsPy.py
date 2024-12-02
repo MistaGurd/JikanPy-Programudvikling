aae2a838c3dd80021cd40785af600b73a40bc3dc
NikolajFisk <morangnikolaj2000@gmail.com>
2024-12-02 15:01:12 +0100

    Har lavet to skræme som kan bevæge sig

---
+++ WindowsPy.py
@@ -0,0 +1,18 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+class SearchWindow(Screen):
+    pass
+class ResultWindow(Screen):
+    pass
+class WindowManager(ScreenManager):
+    pass
+kv = Builder.load_file('Windows.kv')
+class AnimeApp(App):
+    def build(self):
+        return kv
+if __name__ == '__main__':
+    AnimeApp().run()
+

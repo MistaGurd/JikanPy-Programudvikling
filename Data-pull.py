c839259c37389ed1dc86f559c379a03afdf8fc29
MistaGurd <sigoviggo@gmail.com>
2024-12-05 08:40:06 +0100

    Udskrivning af engelsk navn, works!

--- Data-pull.py
+++ Data-pull.py
@@ -1,6 +1,15 @@
 from jikanpy import Jikan
 jikan = Jikan()
 
-anime = jikan.anime(14719)
+anime = jikan.anime(1735)
 
-print(anime)
+
+def findENtitle(anime): # Funktion til at finde den engelske title
+    for title in anime['data']['titles']: # Her graver vi i din data, som jikan.anime giver:
+                                          # Først den overordnede data, derefter titles
+                                          # Hvorefter vi leder efter typen English, da
+                                          # Jikan kan udskrive for adskillige sprog.
+        if title['type']=='English': # Hvis der findes en engelsk titel:
+            return title['title'] # Gem titlen
+
+print(findENtitle(anime)) # Udskriv titlen

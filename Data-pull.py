1468023e44431c8574cc40a03a3fa672f952ede3
MistaGurd <sigoviggo@gmail.com>
2024-12-05 08:49:02 +0100

    Udskrivning af engelsk navn, works!

--- Data-pull.py
+++ Data-pull.py
@@ -1,7 +1,3 @@
-from.kivy.app import app
-from.kivy.uix.widget import widget
-from.kivy.lang import builder
-from.kivy.uix.screenmanager import screenmanager, screen
 from jikanpy import Jikan
 jikan = Jikan()
 
